9bbaf1a19baf8d63729285ffd0948da28e558de5
Kateryna Strough <katerynastrough@hotmail.com>
2019-04-16 02:24:05 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+ num1=42
+ num2=7
+ puts num1/num2
 end
 
 def assign_variable(value)
-  
+  value="Bob"
+  puts value
 end
 
-def argue
-  
+def argue(25)
+  puts argue
 end
 
-def greeting
-
+def greeting(15)
+puts greeting
 end
 
-def return_a_value
-
+def return_a_value(25)
+puts return_a_value
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(3)
+puts last_evaluated_value
 end
 
-def pizza_party
-
+def pizza_party(4)
+puts pizza_party
 end
