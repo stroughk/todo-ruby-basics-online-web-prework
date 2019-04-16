07506a08e1ea89225cbcd16fe79c2122a5b47735
Kateryna Strough <katerynastrough@hotmail.com>
2019-04-16 02:12:53 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+ num1=42
+ num2=7
+ puts num1/num2
 end
 
 def assign_variable(value)
-  
+  value="Bob"
+  puts value
 end
 
 def argue
-  
+  puts argue
 end
 
 def greeting
-
+puts greeting
 end
 
 def return_a_value
-
+print return_a_value.length()
 end
 
 def last_evaluated_value
-
+puts last_evaluated_value
 end
 
 def pizza_party
-
+puts pizza_party
 end
